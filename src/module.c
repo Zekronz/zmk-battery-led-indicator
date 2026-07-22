0c5e13c770f729cf1199acc8606f3a6ebb6b9af8
Zekronz <zekronpvp@gmail.com>
2026-07-22 15:39:23 +0200

    frick u

--- src/module.c
+++ src/module.c
@@ -9,10 +9,11 @@ BUILD_ASSERT(DT_NODE_EXISTS(DT_ALIAS(green_led)), "Alias 'green_led' not found."
 static const struct gpio_dt_spec led_red = GPIO_DT_SPEC_GET(DT_ALIAS(red_led), gpios);
 static const struct gpio_dt_spec led_green = GPIO_DT_SPEC_GET(DT_ALIAS(green_led), gpios);
 
-static void led_proc_thread(void *d0, void *d1, void *d2) {
-    ARG_UNUSED(d0);
-    ARG_UNUSED(d1);
-    ARG_UNUSED(d2);
+//static void led_proc_thread(void *d0, void *d1, void *d2) {
+static int led_proc_thread(void){
+    //ARG_UNUSED(d0);
+    //ARG_UNUSED(d1);
+    //ARG_UNUSED(d2);
 
     if(!device_is_ready(led_red.port)) return;
     if(!device_is_ready(led_green.port)) return;
