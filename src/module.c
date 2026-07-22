62daede1721a5f8825f557c8fbc24ff0891e7657
Zekronz <zekronpvp@gmail.com>
2026-07-22 22:33:30 +0200

    Status test

--- src/module.c
+++ src/module.c
@@ -1,77 +1,95 @@
 #include <zephyr/drivers/gpio.h>
 #include <zephyr/kernel.h>
-//#include <zephyr/drivers/led.h>
-#include <zmk/events/battery_state_changed.h>
+#include <zephyr/device.h>
+//#include <zmk/events/battery_state_changed.h>
 
-BUILD_ASSERT(DT_NODE_EXISTS(DT_NODELABEL(red_led)), "Alias 'red_led' not found.");
-BUILD_ASSERT(DT_NODE_EXISTS(DT_NODELABEL(green_led)), "Alias 'green_led' not found.");
+BUILD_ASSERT(DT_NODE_EXISTS(DT_NODELABEL(red_led)), "Node 'red_led' not found.");
+BUILD_ASSERT(DT_NODE_EXISTS(DT_NODELABEL(green_led)), "Node 'green_led' not found.");
+
+BUILD_ASSERT(DT_NODE_EXISTS(DT_NODELABEL(charge_status)), "Node 'charge_status' not found.");
+BUILD_ASSERT(DT_NODE_HAS_PROP(DT_NODELABEL(charge_status), stat1), "Node 'charge_status.stat1' not found.");
+BUILD_ASSERT(DT_NODE_HAS_PROP(DT_NODELABEL(charge_status), stat2), "Node 'charge_status.stat2' not found.");
 
 static const struct gpio_dt_spec led_red = GPIO_DT_SPEC_GET(DT_NODELABEL(red_led), gpios);
 static const struct gpio_dt_spec led_green = GPIO_DT_SPEC_GET(DT_NODELABEL(green_led), gpios);
 
+static const struct gpio_dt_spec stat1_pin = GPIO_DT_SPEC_GET(DT_NODELABEL(charge_status), stat1);
+static const struct gpio_dt_spec stat2_pin = GPIO_DT_SPEC_GET(DT_NODELABEL(charge_status), stat2);
+
 static struct k_work_delayable led_work;
-static bool led_state = false;
+static struct gpio_callback stat1_cb_data;
+static struct gpio_callback stat2_cb_data;
+
+static bool stat1_enabled = false;
+static bool stat2_enabled = false;
+
+static void update_charge_status(int s1, int s2){
+	if(s1 < 0 || s2 < 0) return;
+
+	stat1_enabled = s1;
+	stat2_enabled = s2;
+
+	gpio_pin_set_dt(&led_red, s1);
+	gpio_pin_set_dt(&led_green, s2);
+}
 
 static void bat_led_work_handler(struct k_work *work){
-    /*int battery_level = zmk_battery_state_of_charge();
+	//k_work_reschedule(&led_work, K_SECONDS(1));
+	update_charge_status(gpio_pin_get_dt(&stat1_pin), gpio_pin_get_dt(&stat2_pin));
+	//@TODO: Handle sleep
+}
 
-    if (battery_level < 30) {
-        // Low battery
-        gpio_pin_set_dt(&led_red, 1);
-        gpio_pin_set_dt(&led_green, 0);
-    } else {
-        // Battery OK
-        gpio_pin_set_dt(&led_red, 0);
-        gpio_pin_set_dt(&led_green, 1);
-    }*/
-
-	if(!led_state){
-		gpio_pin_set_dt(&led_red, 1);
-        gpio_pin_set_dt(&led_green, 0);
-	}else{
-		gpio_pin_set_dt(&led_red, 0);
-        gpio_pin_set_dt(&led_green, 1);
-	}
-
-	led_state = !led_state;
-
-	k_work_reschedule(&led_work, K_SECONDS(1));
+static void stat_cb(const struct device *dev, struct gpio_callback *cb, uint32_t pins){
+	k_work_schedule(&led_work, K_MSEC(20));
 }
 
 static int bat_led_init(void){
     if(!device_is_ready(led_red.port)) return -ENODEV;
     if(!device_is_ready(led_green.port)) return -ENODEV;
-
-    gpio_pin_configure_dt(&led_red, GPIO_OUTPUT_INACTIVE);
-    gpio_pin_configure_dt(&led_green, GPIO_OUTPUT_INACTIVE);
+	if(!device_is_ready(stat1_pin.port)) return -ENODEV;
+	if(!device_is_ready(stat2_pin.port)) return -ENODEV;
 
 	k_work_init_delayable(&led_work, bat_led_work_handler);
-    k_work_schedule(&led_work, K_NO_WAIT);
+
+	int ret;
+
+    ret = gpio_pin_configure_dt(&led_red, GPIO_OUTPUT_INACTIVE);
+	if(ret < 0) return ret;
+
+    ret = gpio_pin_configure_dt(&led_green, GPIO_OUTPUT_INACTIVE);
+	if(ret < 0) return ret;
+
+	ret = gpio_pin_configure_dt(&stat1_pin, GPIO_INPUT);
+	if(ret < 0) return ret;
+
+	ret = gpio_pin_configure_dt(&stat2_pin, GPIO_INPUT);
+	if(ret < 0) return ret;
+
+	update_charge_status(gpio_pin_get_dt(&stat1_pin), gpio_pin_get_dt(&stat2_pin));
+
+	ret = gpio_pin_interrupt_configure_dt(&stat1_pin, GPIO_INT_EDGE_BOTH);
+	if(ret < 0) return ret;
+
+	ret = gpio_pin_interrupt_configure_dt(&stat2_pin, GPIO_INT_EDGE_BOTH);
+	if(ret < 0) return ret;
+
+	if(stat1_pin.port == stat2_pin.port){
+		gpio_init_callback(&stat1_cb_data, stat_cb, BIT(stat1_pin.pin) | BIT(stat2_pin.pin));
+
+		ret = gpio_add_callback(stat1_pin.port, &stat1_cb_data);
+		if(ret < 0) return ret;
+	}else {
+		gpio_init_callback(&stat1_cb_data, stat_cb, BIT(stat1_pin.pin));
+		gpio_init_callback(&stat2_cb_data, stat_cb, BIT(stat2_pin.pin));
+
+		ret = gpio_add_callback(stat1_pin.port, &stat1_cb_data);
+		if(ret < 0) return ret;
+
+		ret = gpio_add_callback(stat2_pin.port, &stat2_cb_data);
+		if(ret < 0) return ret;
+	}
 
     return 0;
 }
 
 SYS_INIT(bat_led_init, APPLICATION, CONFIG_APPLICATION_INIT_PRIORITY);
-
-//static void led_proc_thread(void *d0, void *d1, void *d2) {
-/*static int led_proc_thread(void){
-    //ARG_UNUSED(d0);
-    //ARG_UNUSED(d1);
-    //ARG_UNUSED(d2);
-
-    if(!device_is_ready(led_red.port)) return;
-    if(!device_is_ready(led_green.port)) return;
-
-	int ret;
-	ret = gpio_pin_configure_dt(&led_red, GPIO_OUTPUT_ACTIVE);
-	if(ret < 0) return;
-
-	ret = gpio_pin_configure_dt(&led_green, GPIO_OUTPUT_ACTIVE);
-	if(ret < 0) return;
-
-	gpio_pin_set_dt(&led_red, 1);
-	gpio_pin_set_dt(&led_green, 1);
-}
-
-//K_THREAD_DEFINE(led_proc_id, 1024, led_proc_thread, NULL, NULL, NULL, K_LOWEST_APPLICATION_THREAD_PRIO, 0, 100);
-SYS_INIT(led_proc_thread, APPLICATION, CONFIG_APPLICATION_INIT_PRIORITY);*/
