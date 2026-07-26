f1400a21cf869fcdb024fa0ba2e88d4ca1167d94
Zekronz <zekronpvp@gmail.com>
2026-07-26 23:01:31 +0200

    Stuff

--- src/module.c
+++ src/module.c
@@ -6,8 +6,8 @@
 #include <zmk/event_manager.h>
 #include <zmk/events/usb_conn_state_changed.h>
 #include <zmk/events/activity_state_changed.h>
-#include <zmk/events/battery_state_changed.h>
-#include <zmk/battery.h>
+//#include <zmk/events/battery_state_changed.h>
+//#include <zmk/battery.h>
 #include <zmk/usb.h>
 
 LOG_MODULE_DECLARE(zmk, CONFIG_ZMK_LOG_LEVEL);
@@ -24,7 +24,7 @@ static const struct gpio_dt_spec led_green = GPIO_DT_SPEC_GET(DT_NODELABEL(green
 static const struct gpio_dt_spec stat1_pin = GPIO_DT_SPEC_GET(DT_NODELABEL(charge_status), stat1_gpios);
 static const struct gpio_dt_spec stat2_pin = GPIO_DT_SPEC_GET(DT_NODELABEL(charge_status), stat2_gpios);
 
-static struct k_mutex mutex;
+/*static struct k_mutex mutex;
 
 static struct k_work_delayable init_bat_work;
 static struct k_work_delayable stat_bat_work;
@@ -36,7 +36,7 @@ static volatile bool initialized = false;
 static volatile bool stat1_enabled = false;
 static volatile bool stat2_enabled = false;
 static volatile bool is_active = true;
-static volatile uint8_t bat_level = 100;
+static volatile uint8_t bat_level = 100;*/
 
 /*static void cb_update_leds_work(struct k_work *work){
 	//k_mutex_lock(&mutex, K_FOREVER);
@@ -236,8 +236,8 @@ static int bat_led_init(void){
 	//k_mutex_init(&mutex);
 
 	k_work_init_delayable(&init_bat_work, cb_init_bat_work);
-	k_work_init_delayable(&stat_bat_work, cb_stat_bat_work);
-	k_work_init_delayable(&update_leds_work, cb_update_leds_work);
+	//k_work_init_delayable(&stat_bat_work, cb_stat_bat_work);
+	//k_work_init_delayable(&update_leds_work, cb_update_leds_work);
 
 	int ret;
 	ret = k_work_schedule(&init_bat_work, K_MSEC(200));
